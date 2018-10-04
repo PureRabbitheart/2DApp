a69b412e92b56f6a4110460272f2269a64bfc5c4
PureRabbitHeart <akb48_maeda_oosima_sasaki@yahoo.co.jp>
2018-10-04 16:26:13 +0900

    apkファイルの書き出し設定終了 簡単なUI配置をした

---
+++ 2DApp/Assets/test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void MainScene()
+	{
+		SceneManager.LoadScene("Main");
+	}
+	public void　SettingScene()
+	{
+		SceneManager.LoadScene("Setting");
+	}	
+	public void ExitScene()
+	{
+	　#if UNITY_EDITOR
+   			EditorApplication.isPlaying = false;
+      	 #elif UNITY_STANDALONE
+    		Application.Quit();
+   	 #endif
+	}
+}

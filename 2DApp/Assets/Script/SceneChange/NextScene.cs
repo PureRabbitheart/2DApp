f9993e1bab834170e248b915db7d6a2af10f2620
PureRabbitHeart <akb48_maeda_oosima_sasaki@yahoo.co.jp>
2018-10-05 12:36:44 +0900

    Scene管理 タップ処理作成

---
+++ 2DApp/Assets/Script/SceneChange/NextScene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+
+	[SerializeField]
+	private SceneObject TitleSceneObject;
+	[SerializeField]
+	private SceneObject MainSceneObject;
+	[SerializeField]
+	private SceneObject SettingSceneObject;
+	[SerializeField]
+	private SceneObject SelectSceneObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TitleScene()//タイトルシーン遷移
+	{
+		SceneManager.LoadScene(TitleSceneObject);
+	}
+	public void MainScene()//メインシーン遷移
+	{
+		SceneManager.LoadScene(MainSceneObject);
+	}
+	public void　SettingScene()//設定シーン遷移
+	{
+		SceneManager.LoadScene(SettingSceneObject);
+	}	
+	public void SelectScene()//セレクトシーン遷移
+	{
+	　SceneManager.LoadScene(SelectSceneObject);
+	}
+
+}
